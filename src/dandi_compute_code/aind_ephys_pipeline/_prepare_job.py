3bcaf597aea4ba2cda7aeae2fd6150fe45a9fc0e
Cody Baker <cody.c.baker.phd@gmail.com>
2026-02-05 15:30:03 -0500

    debug

--- src/dandi_compute_code/aind_ephys_pipeline/_prepare_job.py
+++ src/dandi_compute_code/aind_ephys_pipeline/_prepare_job.py
@@ -116,7 +116,7 @@ def prepare_aind_ephys_job(
         config_file_path=str(code_config_file_path),
         pipeline_file_path=str(pipeline_file_path),
         temp_name=temporary_processing_directory.name,
-        done_tracker_file_path=done_tracker_file_path,
+        done_tracker_file_path=str(done_tracker_file_path),
     )
     code_config_file_path.write_text(data=config_file_path.read_text())
 
