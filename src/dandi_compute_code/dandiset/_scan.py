a0c5e044c53e3beface520af0c3588b178eab1fd
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-05-19 23:16:36 -0400

    Refactor Dandi API client creation for sandbox
    Removed api_token parameter when creating Dandi API client for sandbox.

--- src/dandi_compute_code/dandiset/_scan.py
+++ src/dandi_compute_code/dandiset/_scan.py
@@ -17,7 +17,7 @@ _SANDBOX_API_URL = "https://api.sandbox.dandiarchive.org/api"
 def _create_dandi_api_client(*, api_token: str, dandiset_id: str) -> dandi.dandiapi.DandiAPIClient:
     """Create a DANDI API client, routing known sandbox dandiset IDs to the sandbox API."""
     if dandiset_id == _SANDBOX_DANDISET_ID:
-        return dandi.dandiapi.DandiAPIClient(token=api_token, api_url=_SANDBOX_API_URL)
+        return dandi.dandiapi.DandiAPIClient(api_url=_SANDBOX_API_URL)
     return dandi.dandiapi.DandiAPIClient(token=api_token)
 
 
