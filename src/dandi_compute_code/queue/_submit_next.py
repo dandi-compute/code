cd57d7e0d16c19625bbae560ff277d76ee108737
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-05-23 15:03:37 -0400

    Apply suggestion from @CodyCBakerPhD

--- src/dandi_compute_code/queue/_submit_next.py
+++ src/dandi_compute_code/queue/_submit_next.py
@@ -6,6 +6,7 @@ from ._resolve_unsubmitted_attempt_dir import _resolve_unsubmitted_attempt_dir
 from ..aind_ephys_pipeline import submit_job
 
 
+# TODO: make logic even cleaner
 def _submit_next(
     *,
     queue_directory: pathlib.Path,
