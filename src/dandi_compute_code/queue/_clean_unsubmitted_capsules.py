f997ce069f052dca04d190ce1fe153b54e2dc297
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-05-28 20:05:04 -0400

    Refactor _remove_empty_parents function
    Refactor _remove_empty_parents to avoid try/except pattern.

--- src/dandi_compute_code/queue/_clean_unsubmitted_capsules.py
+++ src/dandi_compute_code/queue/_clean_unsubmitted_capsules.py
@@ -8,6 +8,27 @@ from ._remove_empty_parents import _remove_empty_parents
 from ._resolve_unsubmitted_attempt_dir import _resolve_unsubmitted_attempt_dir
 
 
+# TODO: refactor this to not use try/except pattern (shouldn't be necessary)
+def _remove_empty_parents(*, start: pathlib.Path, stop: pathlib.Path) -> None:
+    """
+    Remove empty directories from ``start`` up to but not including ``stop``.
+
+    If ``stop`` is not an ancestor of ``start``, this function returns without
+    modifying the filesystem. Removal stops at the first non-empty directory.
+    """
+    if stop not in start.parents:
+        return
+
+    current = start
+    while current != stop:
+        if not current.exists() or not current.is_dir():
+            break
+        try:
+            current.rmdir()
+        except OSError:
+            break
+        current = current.parent
+
 # TODO: review if the return value here is needed at all
 # TODO: make more efficient in terms of --preserve-tree
 def clean_unsubmitted_capsules(
